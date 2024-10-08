6ef40597c928978b11682575e736da3a573b3876
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2024-10-08 15:39:26 +0100

    Gathering more data from html

--- src/main.py
+++ src/main.py
@@ -23,24 +23,61 @@ with urllib.request.urlopen(url, context=ctx) as response:
 soup = BeautifulSoup(html_text, 'html.parser')
 soup.prettify()
 
-print("All imports of this html file")
+storage = {}
 for link in soup.find_all('a'):
-    # Finds all imports
-    if 'class' in link.attrs and\
-        'Keyword' in link['class'] and\
-        'import' in link.string:
-        package = link.find_next("a")
-        print(link.text, package.text)
+    href_location = ' '.join(
+        link['class']) if 'class' in link.attrs else "None"
+    storage[href_location] = storage.get(href_location, set())
+    storage[href_location].add(link.text)
+    # if 'class' in link.attrs:
+    #     for c in link['class']:
+    #         classes.add(c)
+    #
+    # for class_name in storage.keys():
+    #     if 'class' in link.attrs and\
+    #             class_name in link['class']:
+    #         storage[class_name].add(link.text)
 
-print()
-print("All functions of this html file")
-functions = set()
+for k, v in storage.items():
+    print(f"All {k} found:")
+    for s in v:
+        print(s)
+print("All classes found")
+print('\n'.join(storage.keys()))
+
+
+references = {}
 for link in soup.find_all('a'):
-    # print(len(str(link).strip()) - len(str(link)))
-    # Finds all the functions in file
-    if 'class' in link.attrs and 'Function' in link['class']:
-        functions.add((link.text, link['href']))
-# print(list(soup.children))
-print(list(soup.find('pre').children))
+    href_location = link['href'] if 'href' in link.attrs else "None"
+    href_location = href_location.split("#")[0]
+    references[href_location] = references.get(href_location, set())
+    references[href_location].add(link.text)
+
+for k, v in references.items():
+    print(f"All references to {k} found:")
+    for s in v:
+        print(s)
+# prin()
+# print("All functions of this html file")
+# functions = set()
+# for link in soup.find_all('a'):
+#     # print(len(str(link).strip()) - len(str(link)))
+#     # Finds all the functions in file
+#     if 'class' in link.attrs and 'Function' in link['class']:
+#         functions.add((link.text, link['href']))
+# # print(list(soup.children))
+# # print(list(soup.find('pre').children))
 # for (f, h) in functions:
-#     print(f, h)
+#     print(f)
+# print()
+# print("All Data types of this html file")
+# data_types = set()
+# for link in soup.find_all('a'):
+#     # print(len(str(link).strip()) - len(str(link)))
+#     # Finds all the functions in file
+#     if 'class' in link.attrs and 'Datatype' in link['class']:
+#         data_types.add(link.text)
+# # print(list(soup.children))
+# # print(list(soup.find('pre').children))
+# for f in data_types:
+#     print(f)

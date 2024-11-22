500de02b6b4c575a1b4ad282ce1ef401c8ed6df3
JulianVidal <julividal03@hotmail.com>
2024-11-22 20:12:48 +0000

    Added modules to cli

--- agda_tree/cli.py
+++ agda_tree/cli.py
@@ -5,18 +5,20 @@ import os
 from inspect import signature, getdoc, getmembers, isfunction
 import sys
 import networkx as nx
-import commands
+import def_cmds
+import mod_cmds
 
 arg_help = {
-    "g": "Path to tree (Default: tree.pickle)",
-    "-m": "If set it uses the module version of the command",
+    "g": "Path to tree (Default: ",
     "-output": "Ouput file name",
+    "m": "Module name",
     "d": "Definition name",
     "path": "Path to directory or file",
     "src": "Source definition",
     "dst": "Destination definition",
     "dot_file": "Path to dependency tree dot file",
-    "sexp_dir": "Directory of s-expressions"
+    "sexp_dir": "Directory of s-expressions",
+    "-indirect": "Get indirectly connected nodes"
 }
 
 # TODO: Add topological sort as a query
@@ -29,28 +31,47 @@ agda_tree get_depedencies "InfinitePigeon.Addition._+_"
 """
 def main():
     parser = argparse.ArgumentParser(description="Agda dependencies tree")
-    subparsers = parser.add_subparsers(dest='cmd')
+    tree_parser = parser.add_subparsers(dest='tree')
+
+    commands = {
+        "definition": def_cmds,
+        "module": mod_cmds
+    }
+
+    subparsers = {}
     
-    for cmd, func in getmembers(commands, isfunction):
-        sub = subparsers.add_parser(cmd, help=getdoc(func))
-        # print(cmd, signature(func))
-        # print(cmd, list(signature(func).parameters))
-        for arg, param in signature(func).parameters.items():
-            if arg == "g":
-                sub.add_argument("-g", help=arg_help.get(arg, ""), default="tree.pickle")
-                continue
-            if param.default is not param.empty:
-                action = "store_true" if param.default == False else None
-                arg = "-" + arg
-                sub.add_argument(arg, help=arg_help.get(arg, ""), default=param.default, action=action)
-            else:
-                sub.add_argument(f"{arg}", help=arg_help.get(arg, ""))
+    def_parser = tree_parser.add_parser("definition")
+    subparsers["definition"] = def_parser.add_subparsers(dest='cmd')
+
+    mod_parser = tree_parser.add_parser("module")
+    subparsers["module"] = mod_parser.add_subparsers(dest='cmd')
+
+    for tree in ["definition", "module"]:
+        for cmd, func in getmembers(commands[tree], isfunction):
+            sub = subparsers[tree].add_parser(cmd, help=getdoc(func))
+            # print(cmd, signature(func))
+            # print(cmd, list(signature(func).parameters))
+            for arg, param in signature(func).parameters.items():
+                if arg == "g":
+                    default = "def_tree.pickle"
+                    if tree == "module":
+                        default = "mod_tree.pickle"
+                    sub.add_argument("-g", help=arg_help.get(arg, "") + default + ")", default=default)
+                    continue
+                if param.default is not param.empty:
+                    action = "store_true" if param.default == False else None
+                    arg = "-" + arg
+                    sub.add_argument(arg, help=arg_help.get(arg, ""), default=param.default, action=action)
+                else:
+                    sub.add_argument(f"{arg}", help=arg_help.get(arg, ""))
 
     args = parser.parse_args()
 
-    if hasattr(commands, args.cmd):
+    # print(args)
+    if hasattr(commands[args.tree], args.cmd):
         params = dict(vars(args))
         del params["cmd"]
+        del params["tree"]
         # print(params)
         if "g" in params:
             try:
@@ -59,8 +80,8 @@ def main():
                 print("Couldn't load tree, please use sub-command create_tree first")
                 sys.exit(1)
 
-        print(params)
-        result = getattr(commands, args.cmd)(**params)
+        # print(params)
+        result = getattr(commands[args.tree], args.cmd)(**params)
         if result is not None:
             print("\n".join(list(result)))
     else:
--- agda_tree/parser.py
+++ agda_tree/parser.py
@@ -1,8 +1,14 @@
 from sexpdata import loads, dumps, Symbol
 import json 
 from pathlib import Path
+from tqdm import tqdm
     
-def find_exp(exp, target):
+# FIXME: If there are two lemmas with the same depth, since the number
+# following the definition is removed. These two definitions with the same naem
+# get merged together. To fix this, you would need to either keep the numbers
+# or find the context in which these sub-definitions are beign called or just
+# remove them entirely.
+def find_exp(exp, target, avoid=""):
     found = []
     for e in exp:
         if not isinstance(e, list):
@@ -10,6 +16,9 @@ def find_exp(exp, target):
 
         if e[0] == Symbol(target):
             found.append(e)
+
+        if e[0] == Symbol(avoid):
+            continue
 
         t = find_exp(e, target)
         found.extend(t)
@@ -22,7 +31,9 @@ def parse_files(paths):
     def_types = {}
     def_to_mod = {}
 
-    for path in paths:
+    print()
+    print("Parsing s-expressions")
+    for path in tqdm(list(paths)):
         file = open(path).read()
         sexps = loads(file)
         mod_name = sexps[1][1]
@@ -38,7 +49,7 @@ def parse_files(paths):
             def_name = entry[1][1].split(' ')[0]
             defs[def_name] = list({
                 e[1].split(' ')[0]
-                for e in find_exp(entry, ":name") 
+                for e in find_exp(entry, ":name", avoid=":data") 
                 if e[1].split(' ')[0] != def_name
             })
             def_types[def_name] = list({e[1].split(' ')[0] for e in types})

c97be00eb9c0247c024bd184b5b5d47ab26b199a
JulianVidal <julividal03@hotmail.com>
2025-02-20 07:32:30 +0000

    Fixed unsafe compilation

--- compilation_tests/test_unsafe.py
+++ compilation_tests/test_unsafe.py
@@ -8,23 +8,27 @@ def create_test(g, index_flags, dir):
     # most_descendants = sorted(g.nodes(), key=lambda n: len(nx.descendants(g, n)))
 
     # compile_order = [[[d] for d in most_descendants[-5:]]]
+    nodes = list(g.nodes)
+    split = len(nodes) // 4
+    compile_order =  [[nodes[i:i+split] for i in range(0, len(nodes), split)]]
     # test_generator.generate_test(compile_order, dir)
-    # make_generator.generate_test(compile_order, index_flags, dir)
-    generate_test(g, index_flags, dir)
-    return [[[m] for m in g.nodes]]
+    make_generator.generate_test(compile_order, index_flags, dir)
+    # generate_test(g, index_flags, dir)
+    return compile_order
 
 def generate_test(g, index_flags, dir):
     print(dir)
 
-    makefile = "all: \n"
+    makefile = ""
     for mod in g.nodes():
+        makefile += f"{mod}: \n"
         mod_path = dir / Path(mod.replace(".", "/"))
         if mod_path.with_suffix(".lagda").exists():
             mod_path = mod_path.with_suffix(".lagda")
         else:
             mod_path = mod_path.with_suffix(".agda")
 
-        makefile += f"\tagda {mod_path}\n"
+        makefile += f"\tagda {mod_path}\n\n"
 
     file_name = Path(dir, "compilation.mk")
     file = open(file_name, "w")

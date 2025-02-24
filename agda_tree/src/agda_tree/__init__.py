9d0b3273001c43237ca8aecc146a12a37c881328
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2025-02-24 14:43:41 +0000

    Fixed pyrpoject entry point

---
+++ agda_tree/src/agda_tree/__init__.py
@@ -0,0 +1,48 @@
+from shutil import which
+from pathlib import Path
+import subprocess
+
+from agda_tree import cli
+
+SEXP_REPO = "https://github.com/andrejbauer/agda.git"
+SEXP_DIR = Path("/tmp/agdasexp")
+SEXP_BRNC = "origin/release-2.7.0.1-sexp"
+
+
+def install_agdasexp():
+    print("agdasexp is missing!")
+    ans = input("Do you want to install? [Y/n]\n")
+
+    if ans.lower() in ["y", "ye", "yes", ""]:
+        print("Installing agdasexp")
+        if which("cabal") is None:
+            print("Can't install agdasexp without cabal")
+            return
+
+        print("Cloning s-expressions extractor")
+        subprocess.run(f"git clone {SEXP_REPO} {SEXP_DIR}", shell=True)
+
+        print("Cabal install")
+        cmds = "; ".join([
+            f"cd {SEXP_DIR}",
+            f"git checkout {SEXP_BRNC}",
+            "cabal install . --program-suffix=sexp --overwrite-policy=always"
+        ])
+        subprocess.run(cmds, shell=True, check=True)
+
+        print("Installed succesfully please run program again")
+    else:
+        print("You won't be able to create definition graphs without the s-expressions extractor")
+        cli.main()
+        return
+
+
+def main():
+    if which("agdasexp") is not None:
+        cli.main()
+    else:
+        install_agdasexp()
+
+
+if __name__ == "__main__":
+    main()
--- agda_tree/src/agda_tree/__main__.py
+++
@@ -1,3 +0,0 @@
-if __name__ == "__main__":
-    from agda_tree.cli import main
-    main()
